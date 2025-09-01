3b484373fd5a893e409f737ccdf777b558d62c83
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2025-09-01 14:54:17 +0200

    soppa m

--- src/App.java
+++ src/App.java
@@ -6,7 +6,8 @@ public class App {
      int val = 0;
      int kuponger  = 10;
      int längd = 0;
-     int ålder = 0;
+     int alder = 0;
+     boolean vuxen;
 
 
      System.out.println("Välkommen, välj attraktion!");
@@ -43,8 +44,15 @@ public class App {
         }
     }
 
-    if (val == 2){
+    else if (val == 2){
 
+        System.out.println(
+
+        """
+            Längdkrav: 140cm
+            Ålderskrav: 8 år    
+                """
+        );
         while(true){
 
         try{
@@ -55,6 +63,7 @@ public class App {
         catch(Exception e)
         {
         System.out.println("Inga bokstäver");
+        tb.nextLine();
         continue;
         }
 
@@ -62,7 +71,41 @@ public class App {
 
         if(längd < 140){
             System.out.println("Du är för kort.");
+            return;
         }
+        else{
+            System.out.println("Tillräcklig längd");
+        }
+
+        while (true) {
+            
+        
+        try{
+            System.out.print("Ange ålder:");
+            alder = tb.nextInt();
+            break;
+            }
+            catch(Exception e)
+            {
+            System.out.println("Inga bokstäver");
+            tb.nextLine();
+            continue;
+            }
+    
+        }
+    
+            if(alder < 8){
+                System.out.println("Du är för ung.");
+                return;
+            }
+            else{
+                System.out.println("Tillräcklig ålder");
+                
+            }
+
+
+        
+    
 
 
         if (kuponger < 2){
@@ -74,17 +117,97 @@ public class App {
             System.out.println("Du har skoj i flygande mattan");
         }
     }
-    if (val == 3){
-        if (kuponger < 1){
-            System.out.println("Du har inte råd att åka cirkuskarusellen.");
+    
+    else if (val == 3){
+        while(true){
+            System.out.println("""
+                    
+            Längd med vuxen:95cm    Ålder med vuxen: 4 år
+            Längd utan vuxen: 120cm     ålder utan vuxen:6 år
+                    """);
+
+            while(true){
+            System.out.println("Åker du med vuxen? :");
+            String svar = tb.nextLine();
+
+                if (svar.equalsIgnoreCase("ja")){
+                    vuxen = true;
+                    break;
+                }
+                else if(svar.equalsIgnoreCase("nej")){
+                    vuxen = false;
+                    break;
+                }
+                else{
+                    System.out.println("Du har gjort fel");
+                }
+
+            }
+
+
+            try{
+            System.out.print("Ange längd:");
+            längd = tb.nextInt();
+            break;
+            }
+            catch(Exception e)
+            {
+            System.out.println("Inga bokstäver");
+            tb.nextLine();
+            continue;
+            }
+    
+            }
+    
+            if(längd < 140){
+                System.out.println("Du är för kort.");
+                return;
+            }
+            else{
+                System.out.println("Tillräcklig längd");
+            }
+    
+            while (true) {
+                
+            
+            try{
+                System.out.print("Ange ålder:");
+                alder = tb.nextInt();
+                break;
+                }
+                catch(Exception e)
+                {
+                System.out.println("Inga bokstäver");
+                tb.nextLine();
+                continue;
+                }
+        
+            }
+        
+                if(alder < 8){
+                    System.out.println("Du är för ung.");
+                    return;
+                }
+                else{
+                    System.out.println("Tillräcklig ålder");
+                }
+    
+    
+            
+        
+    
+    
+            if (kuponger < 2){
+                System.out.println("Du har inte råd att åka flygande mattan.");
+            }
+            else if(kuponger >= 2){
+                System.out.println("Du spenderar 2 kuponger på flygande mattan..");
+                kuponger -= 2;
+                System.out.println("Du har skoj i flygande mattan");
+            }
         }
-        else if(kuponger >= 1){
-            System.out.println("Du spenderar en biljett på cikrusekarusellen.");
-            kuponger --;
-            System.out.println("Du har skoj i karusellen");
-        }
-    }
-    if (val == 4){
+    
+   else if (val == 4){
         if (kuponger < 1){
             System.out.println("Du har inte råd att åka cirkuskarusellen.");
         }
